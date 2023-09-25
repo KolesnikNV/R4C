d710dd9fc2893a1ab66eb5e651cd2fa097d81944
Nikita Kolesnik <118283668+KolesnikNV@users.noreply.github.com>
2023-09-25 18:12:11 +0400

    Task 2 done

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from robots.views import CreateRobotView
+from utils.reports import GenerateExcelReportView
 
 urlpatterns = [
     path("robots/", CreateRobotView.as_view(), name="robots"),
+    path("download/", GenerateExcelReportView.as_view(), name="exel"),
 ]
--- utils/reports.py
+++ utils/reports.py
@@ -1,19 +1,48 @@
+from rest_framework.views import APIView
+from datetime import timedelta
 import openpyxl
-from datetime import datetime, timedelta
 from robots.models import Robot
+import os
+from django.http import HttpResponse
+from django.utils import timezone
 
 
-def generate_excel_report():
-    end_date = datetime.now()
-    start_date = end_date - timedelta(days=7)
-    robots_data = Robot.objects.filter(created__range=[start_date, end_date])
+class GenerateExcelReportView(APIView):
+    def get(self, request):
+        end_date = timezone.now()
+        start_date = end_date - timedelta(days=7)
+        robot_models = Robot.objects.values("model").distinct()
+        workbook = openpyxl.Workbook()
 
-    workbook = openpyxl.Workbook()
-    worksheet = workbook.active
-    worksheet.title = "Отчет по производству"
-    worksheet.append(["Модель", "Версия", "Количество за неделю"])
+        for model in robot_models:
+            model_name = model["model"]
+            model_data = Robot.objects.filter(
+                model=model_name, created__range=(start_date, end_date)
+            )
 
-    for robot in robots_data:
-        worksheet.append([robot.model, robot.version, 1])
+            if model_data.exists():
+                worksheet = workbook.create_sheet(title=model_name)
+                worksheet.append(["Модель", "Версия", "Количество за неделю"])
 
-    return workbook
+                version_counts = {}
+                for data in model_data:
+                    version = data.version
+                    if version in version_counts:
+                        version_counts[version] += 1
+                    else:
+                        version_counts[version] = 1
+
+                for version, count in version_counts.items():
+                    worksheet.append([model_name, version, count])
+
+        temp_file_path = f"excel_report_{start_date.strftime('%Y-%m-%d')}.xlsx"
+        workbook.save(temp_file_path)
+
+        response = HttpResponse(content_type="application/octet-stream")
+        response["Content-Disposition"] = f'attachment; filename="{temp_file_path}"'
+        response["Content-Length"] = os.path.getsize(temp_file_path)
+
+        with open(temp_file_path, "rb") as excel_file:
+            response.write(excel_file.read())
+
+        return response
