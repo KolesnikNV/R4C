25fccd91ff59723bf0b60b8fdb0380dc0015ab62
Nikita Kolesnik <118283668+KolesnikNV@users.noreply.github.com>
2023-09-26 18:55:55 +0400

    Little change in Celery

--- orders/tasks.py
+++ orders/tasks.py
@@ -1,19 +1,21 @@
 from celery import shared_task
 from django.core.mail import send_mail
 from django.conf import settings
-from utils.models_utils import filter_model
 from robots.models import Robot
+from django.utils import timezone
 
 
 @shared_task
 def send_notification_email(email, robot_serial):
-    robot_available = filter_model(Robot, serial=robot_serial)
-    model, version = robot_serial.split("-")
-    if robot_available:
+    if robots_available := Robot.objects.filter(
+        available_date__gte=timezone.now() - timezone.timedelta(hours=12),
+        serial=robot_serial,
+    ):
         subject = "Робот доступен"
+        model, version = robot_serial.split("-")
         message = f"Добрый день!\nНедавно вы интересовались нашим роботом модели {model} версии {version}. \nЭтот робот теперь в наличии. Если вам подходит этот вариант - пожалуйста, свяжитесь с нами."
         from_email = settings.DEFAULT_FROM_EMAIL
-        recipient_list = [email]
+        recipient_list = email
         send_mail(
             subject, message, from_email, recipient_list, fail_silently=False
         )
--- utils/celery.py
+++ utils/celery.py
@@ -1,7 +1,6 @@
-from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-
+from django.utils import timezone
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "R4C.settings")
 
@@ -9,5 +8,12 @@ app = Celery("R4C")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.conf.broker_url = "pyamqp://guest@localhost//"
 app.conf.result_backend = "rpc://"
+
+app.conf.beat_schedule = {
+    "check-and-send-notifications": {
+        "task": "orders.send_notification_email",
+        "schedule": timezone.timedelta(hours=12),
+    },
+}
 
 app.autodiscover_tasks()
